64cf88793854e2518417ae9e002d6ed2d0bd1c3a
jdcr1425 <juandavidcr7@gmail.com>
2020-08-14 01:55:26 -0500

    added loan model and structured routes model

---
+++ src/models/Loan.js
@@ -0,0 +1,56 @@
+const validator = require('validator');
+const mongoose = require('mongoose');
+const { Decimal128 } = require('mongodb');
+
+let validLoanType = {
+    values: ['DAILY', 'MONTHLY', 'WEEKLY', 'BIWEEKLY'],
+    message: '{VALUE} is not a valid loan type'
+}
+
+
+let Schema = mongoose.Schema;
+
+let LoanSchema = new Schema({
+    id_client: {
+        type: Schema.Types.ObjectId,
+        ref: "Client",
+        required: [true, 'The client is required']
+    },
+    amount: {
+        type: Decimal128,
+        required: [true, 'The amount is required']
+    },
+    fee: {
+        type: Decimal128,
+        required: [true, 'The fee is required'],
+    },
+    loanType: {
+        type: String,
+        default: 'DAILY',
+        enum: validLoanType,
+        required: true
+    },
+    interestPorcent: {
+        type: Number,
+        required: [true, 'The interest porcent is required']
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    coSigner: {
+        type: Schema.Types.ObjectId,
+        ref: "Client",
+        required: false
+    },
+    finishLoanDate: {
+        type: Date,
+        required: [true, 'The finish loan date is required']
+    }
+
+},
+    { timestamps: { createdAt: 'created_at' } }
+
+);
+
+module.exports = mongoose.model('Loan', LoanSchema);
---
+++ src/routes/loans.js
@@ -0,0 +1,10 @@
+const LoansController = require("../controllers/LoansController");
+
+//Clients routes
+module.exports = app => {
+    app.get("/loans", LoansController.index);
+    app.post("/loans", LoansController.createLoan);
+    app.get("/loans/:id", LoansController.oneLoan);
+    app.put("/loans/:id", LoansController.updateLoan);
+    app.delete("/loans/:id", LoansController.deleteLoan);
+};
